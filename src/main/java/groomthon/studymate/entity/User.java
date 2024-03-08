55e2c9f469818ef1e2b383857154296c9a417958
kwjdgh1267 <kwjdgh1267@gmail.com>
2024-03-08 21:31:39 +0900

    소셜로그인 완성

--- src/main/java/groomthon/studymate/entity/User.java
+++ src/main/java/groomthon/studymate/entity/User.java
@@ -1,7 +1,9 @@
 package groomthon.studymate.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
@@ -10,20 +12,52 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue
     @Column(name = "user_id")
     private Long id;
-
-    private String email;
-
-
     @OneToMany(mappedBy = "user")
     private List<Heart> hearts = new ArrayList<>();
 
     @OneToMany(mappedBy = "user")
     private List<Comment> comments = new ArrayList<>();
+
+
+    //추가
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 
 
 }
